e27e2a6e1e12108ed6b2373fc3077029880ff3f0
harshithacharya632-source <harshithacharya632@gmail.com>
2025-09-27 15:03:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,7 +23,7 @@ else:
 URL = environ.get("URL", "https://goflix-link.onrender.com")
 
 # Admins, Channels & Users
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003059886878'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 
 # MongoDB information
